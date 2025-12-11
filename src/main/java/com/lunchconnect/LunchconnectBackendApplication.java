a7913242fee5e09660f151f5163238f4710f345c
Gustavo Inga <gustavoa_it@hotmail.com>
2025-12-11 02:06:29 -0500

    porfa ya ya

--- src/main/java/com/lunchconnect/LunchconnectBackendApplication.java
+++ src/main/java/com/lunchconnect/LunchconnectBackendApplication.java
@@ -1,14 +1,21 @@
 package com.lunchconnect;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class LunchconnectBackendApplication {
+    @PostConstruct
+    public void enableThreadLocalContext() {
+        // Asegura que el contexto de seguridad permanezca en el hilo de la solicitud.
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_THREADLOCAL);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LunchconnectBackendApplication.class, args);
